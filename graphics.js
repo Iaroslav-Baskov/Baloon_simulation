119068632d4143cbb33ffd7e2a83904294100a25
Iaroslav-Baskov <ymbaskov@gmail.com>
2024-11-18 11:50:09 +0200

    Update1.js

--- graphics.js
+++ graphics.js
@@ -61,7 +61,7 @@ function skyColor(angularDistance, airMass,I0=1,sigma =1.005) {
           var airmass=R/yAtm*Math.sqrt(Math.cos(z)**2+2*yAtm/R+(yAtm/R)**2)-R/yAtm*Math.cos(z);
           if(y>horyzont*height/aHeight+height/2){
             airmass=data["height"]/Atm;
-            dist=z;
+            dist=90-sunY/width*aWidth;
           }
           if(horyzont*height/aHeight+height/2<sunY){
             ctx.fillStyle=skyColor(dist,airmass*(yAtm/Atm),1,1.005+0.5*(sunY-horyzont*height/aHeight-height/2)/height*aHeight/90);
