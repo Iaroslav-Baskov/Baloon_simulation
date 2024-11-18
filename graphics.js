9718729b5b4b6c0078617ac47b938659db7444bb
Iaroslav-Baskov <ymbaskov@gmail.com>
2024-11-18 12:13:36 +0200

    Update graphics.js

--- graphics.js
+++ graphics.js
@@ -52,14 +52,14 @@ function skyColor(angularDistance, airMass,I0=1,sigma =1.005) {
   function drawAtmosphere(step=5){
     horyzont=Math.acos(R/(R+data['height']))/Math.PI*180;
     ctx.fillStyle="black";
-    ctx.fillRect(0,0,width,-horyzont*height/aHeight+height/2);
+    ctx.fillRect(0,0,width,horyzont*height/aHeight+height/2);
     for(var x=0;x<width;x+=step){
       for(var y=0;y<height;y+=step){
           var dist=Math.sqrt((x-sunX)**2+(y-sunY)**2)/width*aWidth;
           var z=(90-(height/2-y)/height*aHeight)/180*Math.PI;
           var yAtm=Atm-data["height"];
           var airmass=R/yAtm*Math.sqrt(Math.cos(z)**2+2*yAtm/R+(yAtm/R)**2)-R/yAtm*Math.cos(z);
-          if(y>-horyzont*height/aHeight+height/2){
+          if(y>horyzont*height/aHeight+height/2){
             airmass=data["height"]/Atm/Math.sin((y-height/2)/height*aHeight/180*Math.PI);
             dist=90-sunY/width*aWidth;
           }
